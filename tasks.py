4f3a6c2bc897e98c19f3dcbf9df71659cb0a14b9
Stefane Fermigier <sf@abilian.com>
2025-01-31 08:57:19 +0100

    deployment

--- tasks.py
+++ tasks.py
@@ -1,13 +1,23 @@
-from invoke import task
+from invoke import task, Context
 
 USER = "web"
-HOST = "lab.abilian.com"
-PATH = "/srv/web/lab.abilian.com"
+NAME = "lab.abilian.com"
 
+# HOST = "lab.abilian.com"
+# PATH = "/srv/web/lab.abilian.com"
+HOST = "trunks3.abilian.com"
+PATH = f"/home/web/{NAME}/"
 
-# HOST = "trunks3.abilian.com"
-# PATH = "/home/web/lab.abilian.com/"
 
 @task
 def deploy(c):
     c.run(f"rsync -e ssh --delete-after -avz site/ {USER}@{HOST}:{PATH}/")
+
+
+@task
+def setup_nginx(c: Context):
+    c.run(f"scp -r nginx.conf root@{HOST}:/etc/nginx/sites-available/{NAME}.conf")
+    c.run(f"ssh root@{HOST} ln -sf /etc/nginx/sites-available/{NAME}.conf /etc/nginx/sites-enabled/{NAME}.conf")
+    c.run(f"ssh root@{HOST} mkdir -p /var/log/nginx/{NAME}")
+    c.run(f"ssh root@{HOST} nginx -t")
+    c.run(f"ssh root@{HOST} systemctl restart nginx")
